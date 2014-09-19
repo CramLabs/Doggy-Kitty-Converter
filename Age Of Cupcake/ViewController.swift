f40c4a0f27cd2238fbd97f4845eb4d3be70971f0
Alex Tuazon <trisoleil@gmail.com>
2014-09-19 13:09:31 -0700

    Trying to make the app not crash using an if statement if user does not enter anything in the textFields

--- Age Of Cupcake/ViewController.swift
+++ Age Of Cupcake/ViewController.swift
@@ -30,13 +30,41 @@ class ViewController: UIViewController {
 
     @IBAction func dogYearsButtonPressed(sender: AnyObject) {
             // Dog Button Code
+            let inputFromUserForDog = dogYearsTextField.text
+            let turnMeIntoAnInteger = inputFromUserForDog.toInt()
+            var integerFromTextField = turnMeIntoAnInteger!
+            let globalAge = 7
+            integerFromTextField *= globalAge
+            displayYourDogYears.hidden = false
+            let stringWithUpdatedAge = "You are " + "\(integerFromTextField)" + " in dog years"
+            displayYourDogYears.text = stringWithUpdatedAge
+            //turnMeIntoAnInteger is used to equal the globalAge
+            // Code for when user forgot to put anything it does not crash but just displays the global age variable.
+            if stringWithUpdatedAge == "" {
+                displayYourDogYears.text = "\(globalAge)"
+            }
+        
+        
+        
+        
         
         }
     
     @IBAction func catYearsButtonPressed(sender: AnyObject) {
             // Cat Button Code
-        
-
+            let inputFromUserForCat = catYearsTextField.text
+            let turnMeIntoAnInteger = inputFromUserForCat.toInt()
+            var integerFromTextField = turnMeIntoAnInteger!
+            let globalAge = 7
+            integerFromTextField *= globalAge
+            displayYourCatYears.hidden = false
+            let stringWithUpdatedAge = "You are " + "\(integerFromTextField)" + " in cat years"
+            displayYourCatYears.text = stringWithUpdatedAge
+            //turnMeIntoAnInteger is used to equal the globalAge
+            // Code for when user forgot to put anything it does not crash but just displays the global age variable.
+            if stringWithUpdatedAge == "" {
+                displayYourCatYears.text = "\(globalAge)"
+            }
         }
 
 
